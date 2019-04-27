d537da8e278465b311f1025453fe35674565569f
lsamano <leizl.samano@gmail.com>
2019-04-27 11:18:57 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,19 @@
-// code solution here
+process.argv.shift()
+process.argv.shift()
+
+if (process.argv.length < 2) {
+  console.log("Please give two arguments.")
+} else if (isNaN(parseInt(process.argv[0])) || isNaN(parseInt(process.argv[1]))) {
+  console.log("Please enter only numbers.")
+} else {
+  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  for(let i=0; i < parseInt(process.argv[1]); i++){
+    let pw = ""
+    for (let x = 0; x < parseInt(process.argv[0]); x++) {
+      pw += charset[Math.floor(Math.random() * charset.length)]
+    }
+    console.log(pw)
+  }
+}
+
+process.exit(0)
